221280af68b54de5780c2854b4347fa5de344aa2
Levy <golvwei@gmail.com>
2024-04-19 22:30:37 +0800

    feat: Add pagination

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
-    // means that all incoming requests will be validated by the ValidationPipe
     new ValidationPipe({
-      whitelist: true, // means that all incoming requests will be stripped of any properties that do not have any decorators
-      forbidNonWhitelisted: true, // means that if a request comes in with properties that do not have any decorators, the request will be rejected
-      transform: true, // means that all incoming request payloads will be transformed into instances of the DTO classes they are bound to
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
     }),
   );
   await app.listen(8080);
